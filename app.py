5972e1ed1db9267ddf3d1783cb882b484a0aebd2
tonygms2 <tonygms2@gmail.com>
2024-07-04 05:24:18 -0400

    Added new route

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from datetime import datetime,timedelta
 import os
+from random import randint
 
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify,request
 from flask_mysqldb import MySQL
 import requests
-from dotenv import load_dotenv
-
-load_dotenv()
+# from dotenv import load_dotenv
+#
+# load_dotenv()
 
 app = Flask(__name__)
 
@@ -16,19 +19,15 @@ app.config['MYSQL_DB'] = 'movie_ticketing'
 
 mysql = MySQL(app)
 
-# OMDb API key
-OMDB_API_KEY = os.getenv('OMDB_API_KEY')
-
-print(OMDB_API_KEY)
+OMDB_API_KEY = 'f2e94d5b'
 
 movies_list = [
     'The Shawshank Redemption', 'The Godfather', 'The Dark Knight', 'Pulp Fiction',
     'Schindler\'s List', 'The Lord of the Rings: The Return of the King', 'Fight Club',
     'Forrest Gump', 'Inception', 'The Matrix', 'Goodfellas', 'The Empire Strikes Back',
     'The Silence of the Lambs', 'Saving Private Ryan', 'The Green Mile', 'Interstellar',
-    'Parasite', 'Gladiator', 'The Departed', 'The Prestige'
+    'Parasite', 'Gladiator', 'The Departed', 'The Prestige', 'Scarface', 'Paglu 2','The Big Bang Theory'
 ]
-
 
 @app.route('/fetch_movies', methods=['GET'])
 def fetch_movies():
@@ -46,21 +45,23 @@ def fetch_movies():
             imdb_rating = data.get('imdbRating')
             poster_url = data.get('Poster')
             duration = data.get('Runtime')
-
+            description = data.get('Plot')
             cursor.execute("""
-                INSERT INTO movies (title, genre, director, release_year, imdb_rating, poster_url, duration)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                INSERT INTO movies (title, genre, director, release_year, imdb_rating, poster_url, duration, description)
+                VALUES (%s, %s, %s, %s, %s, %s, %s,%s)
                 ON DUPLICATE KEY UPDATE
                 genre=VALUES(genre), director=VALUES(director), release_year=VALUES(release_year),
-                imdb_rating=VALUES(imdb_rating), poster_url=VALUES(poster_url), duration=VALUES(duration)
-            """, (title, genre, director, release_year, imdb_rating, poster_url, duration))
+                imdb_rating=VALUES(imdb_rating), poster_url=VALUES(poster_url), duration=VALUES(duration), description=VALUES(description)
+            """, (title, genre, director, release_year, imdb_rating, poster_url, duration, description))
 
     mysql.connection.commit()
     cursor.close()
 
     return jsonify({"message": "Movies data inserted/updated successfully!"})
+#endregion
 
-
+#region API ROUTES
+#route to fetch list of movies from the DB
 @app.route('/movies', methods=['GET'])
 def get_movies():
     cursor = mysql.connection.cursor()
@@ -85,25 +86,57 @@ def get_movies():
     return jsonify(movies)
 
 
-@app.route('/shows', methods=['GET'])
-def get_shows():
+@app.route('/showtimes/<int:movie_id>', methods=['GET'])
+def get_showtimes(movie_id):
     cursor = mysql.connection.cursor()
-    cursor.execute('SELECT * FROM showtimes')
-    rows = cursor.fetchall()
+    cursor.execute('SELECT * FROM showtimes WHERE movie_id = %s', (movie_id,))
+    showtimes = cursor.fetchall()
     cursor.close()
+    showtimes_list = []
+    for showtime in showtimes:
+        showtimes_list.append({
+            "id": showtime[0],
+            "movie_id": showtime[1],
+            "showtime": showtime[2]
+        })
 
-    # Map each row to the desired JSON structure
-    shows = []
-    for row in rows:
-        show = {
-            'id': row[0],
-            'movie_id': row[1],
-            'showtime': row[2].strftime('%Y-%m-%d %H:%M:%S')
-        }
-        shows.append(show)
+    return jsonify(showtimes_list)
 
-    return jsonify(shows)
+@app.route('/book', methods=['POST'])
+def book_tickets():
+    data = request.get_json()
+    showtime_id = data['showtime_id']
+    user_id = data['user_id']
+    seats = data['seats']
+    cursor = mysql.connection.cursor()
+    cursor.execute("INSERT INTO bookings (showtime_id,user_id,seats) VALUES (%s,%s,%s)",
+                   (showtime_id,user_id,seats))
+    mysql.connection.commit()
+    cursor.close()
+    return jsonify({'message': "Your booking has been successfully submitted!"})
 
+# @app.route('/generate_showtimes', methods=['GET'])
+# def generate_showtimes():
+#     cursor = mysql.connection.cursor()
+#     cursor.execute('SELECT id FROM movies')
+#     movie_ids = cursor.fetchall()
+#
+#     now = datetime.now()
+#
+#     for movie_id in movie_ids:
+#         movie_id = movie_id[0]
+#         for _ in range(5):  # Generate 5 showtimes per movie
+#             showtime = now + timedelta(days=randint(0, 7), hours=randint(8, 23))
+#             cursor.execute("""
+#                 INSERT INTO showtimes (movie_id, showtime)
+#                 VALUES (%s, %s)
+#             """, (movie_id, showtime))
+#             mysql.connection.commit()
+#
+#     cursor.close()
+#     return jsonify({"message": "Showtimes generated successfully!"})
+
+#endregion
 
 if __name__ == '__main__':
     app.run(debug=True)
